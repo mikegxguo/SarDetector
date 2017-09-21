9b469edb488bc0471c8b27f3ab0381e99cd28f7a
mike.gou <gxgou.gou@mic.com.tw>
2017-09-21 15:56:37 +0800

    First version

---
+++ src/com/mitac/sar/SarDetectorActivity.java
@@ -0,0 +1,165 @@
+package com.mitac.sar;
+
+//import java.math.*;
+import java.text.DecimalFormat;
+import java.io.File;
+import java.io.FileOutputStream;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.provider.Settings;
+import android.provider.Settings.SettingNotFoundException;
+import android.os.IPowerManager;
+//import android.os.Parcel;
+//import android.os.Parcelable;
+import android.os.PowerManager;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.os.SystemProperties;
+
+import android.os.UEventObserver;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.hardware.SystemSensorManager;
+
+public class SarDetectorActivity extends Activity {
+
+	public LinearLayout linearLayout;
+	private TextView mPSensor = null;
+        //private TextView mBrightness = null;
+    private boolean bSarExist = false;
+    // The sensor manager.
+    private SensorManager mSensorManager;
+
+    // The light sensor, or null if not available or needed.
+    private Sensor mLightSensor;
+
+    // Light sensor event rate in milliseconds.
+    private static final int LIGHT_SENSOR_RATE_MILLIS = 1000;
+    private float lux;
+    // If true, enables the use of the screen auto-brightness adjustment setting.
+    //private /*static final*/ boolean USE_SCREEN_AUTO_BRIGHTNESS_ADJUSTMENT =
+    //        PowerManager.useScreenAutoBrightnessAdjustmentFeature();
+
+    private  int mScreenBrightnessMinimum;
+    private  int mScreenBrightnessMaximum;
+
+    private static final int REFRESH = 0x1234;
+    private Handler hRefresh = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            switch(msg.what){
+                case REFRESH:
+                if(bSarExist) {
+                    if(mPSensor!=null) mPSensor.setText("Person is detected!!!");
+                    linearLayout.setBackgroundColor(Color.RED);
+                    SystemProperties.set("ril.psensor.event.active", "1");
+                } else {
+                    if(mPSensor!=null) mPSensor.setText("Person is out!!!");
+                    linearLayout.setBackgroundColor(Color.BLACK);
+                    SystemProperties.set("ril.psensor.event.active", "0");
+                }
+/*
+if(mPSensor!=null) {
+    DecimalFormat fnum = new DecimalFormat("##0.00"); 
+    String dd=fnum.format(lux); 
+    mPSensor.setText("Lux: "+dd);
+    mPSensor.setTextSize(200);
+}
+*/
+                break;
+                default: break;
+            }
+        }
+    };
+	
+    private UEventObserver mWwanObserver = new UEventObserver() {
+        @Override
+        public void onUEvent(UEventObserver.UEvent event) { //wakesource=sar
+            boolean waked = "sar".equals(event.get("wakesource")) ? true : false;
+            if (waked) {
+                if("detect".equals(event.get("status")))
+                    bSarExist = true;
+                else
+                    bSarExist = false;
+            }
+            hRefresh.sendEmptyMessage(REFRESH);
+        }
+    };
+/*
+    private final SensorEventListener mLightSensorListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+             lux = event.values[0];
+             hRefresh.sendEmptyMessage(REFRESH);
+       }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            // Not used.
+        }
+    };
+*/
+    public void SetData(String fileName, String message)
+    {
+	try{
+	    File file = new File(fileName);
+	    FileOutputStream fout = new FileOutputStream(file);
+	    byte[] bytes = message.getBytes();
+	    fout.write(bytes);
+	    fout.close();
+	} catch (Exception e){
+	    e.printStackTrace();
+	}
+    }
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        mPSensor = (TextView)findViewById(R.id.info);
+        //mBrightness = (TextView)findViewById(R.id.brightness);
+        linearLayout=(LinearLayout)findViewById(R.id.main); 
+        mWwanObserver.startObserving("SUBSYSTEM=platform");
+
+        //SystemProperties.set("ril.psensor.event.pop", "1");
+        //SystemProperties.set("ril.psensor.event.active", "0");
+
+        //mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        //mLightSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+/*
+        PowerManager pm = (PowerManager)getSystemService(Context.POWER_SERVICE);
+        mScreenBrightnessMinimum = pm.getMinimumScreenBrightnessSetting();
+        mScreenBrightnessMaximum = pm.getMaximumScreenBrightnessSetting();
+*/
+        //SetData("/sys/module/lte_power/parameters/lte_enable","1");
+        SetData("/sys/module/lte_power/parameters/psensor_event_pop","1");
+    }
+
+        @Override
+        public void onResume() {
+                super.onResume();
+              //mSensorManager.registerListener(mLightSensorListener, mLightSensor,
+              //          LIGHT_SENSOR_RATE_MILLIS * 1000, hRefresh);
+
+        }
+
+        @Override
+        public void onPause() {
+                super.onPause();
+
+                //mSensorManager.unregisterListener(mLightSensorListener);
+        }
+
+}
