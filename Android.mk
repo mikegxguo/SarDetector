c36156e9f8fa554cb579f6135b28271afdf7f320
mike.gou <gxgou.gou@mic.com.tw>
2019-05-09 13:41:43 +0800

    [SDK] Add the configuration to use hide API in SDK. [Description] Android9

--- Android.mk
+++ Android.mk
@@ -9,6 +9,7 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := SarDetector
+LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_CERTIFICATE := platform
 
 #LOCAL_PROGUARD_FLAG_FILES := proguard.flags
